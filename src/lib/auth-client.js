999fb239e36b0f424fdb13ee617f7b5b7f5a6cd0
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-11-15 10:33:30 +0530

    upadted

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,4 +1,8 @@
 import { createAuthClient } from "better-auth/react";
+
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_API_URL
+      : "http://localhost:3000",
 });
